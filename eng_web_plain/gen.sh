087df70e8af72d91e738b00ccb252b02689b1971
Milan Straka <fox@ucw.cz>
2015-10-13 14:39:12 +0200

    Script for creating plain text corpus.

---
+++ eng_web_plain/gen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sed 's#</*[^>]*>##g' ../eng_web_tbk/data/answers/source/source_original/*.xml >answers.txt
+sed '' ../eng_web_tbk/data/email/source/source_original/*.txt >email.txt
+sed 's#</*[^>]*>##g; s/&gt;/>/g; s/&lt;/</g; s/&amp;/\&/g; s/&quot;/"/g' ../eng_web_tbk/data/newsgroup/source/source_original/*.sgm >newsgroup.txt
+sed '' ../eng_web_tbk/data/reviews/source/source_original/*.txt >reviews.txt
+sed 's#</*[^>]*>##g; s/&gt;/>/g; s/&lt;/</g; s/&amp;/\&/g; s/&quot;/"/g' ../eng_web_tbk/data/weblog/source/source_original/*.sgm >weblog.txt
