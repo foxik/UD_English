f45bc400caecc740a3acde328612a4aa31c79faf
Milan Straka <fox@ucw.cz>
2015-10-13 15:32:04 +0200

    Implement sentence searching in the paragraph.

--- merge.pl
+++ merge.pl
@@ -8,11 +8,18 @@ use open qw(:std :utf8);
 my @conllu;
 
 my %sentences;
+
 my ($sentence, $sentence_start) = ("", 0);
-while (<STDIN>) {
+while (defined ($_ = <STDIN>) || length $sentence) {
   chomp;
+
   my @parts = split /\t/;
-  if (/^$/) {
+
+  # Deal with -[LR]RB- in Form and Lemma columns.
+  @parts && $parts[1] =~ s/-LRB-/(/g && $parts[2] =~ s/-lrb-/(/g;
+  @parts && $parts[1] =~ s/-RRB-/)/g && $parts[2] =~ s/-rrb-/)/g;
+
+  if (!@parts) {
     if (length $sentence) {
       my $tokens = join " ", map {$_->[1]} @conllu[$sentence_start..$#conllu];
       warn "Multiple occurrences of sentence '$sentence' with different tokens $tokens and $sentences{$sentence}->{tokens}" if exists $sentences{$sentence} && $tokens ne $sentences{$sentence}->{tokens};
@@ -27,7 +34,36 @@ while (<STDIN>) {
   push @conllu, [@parts];
 }
 
+# Generate all sentence prefixes
+my %sentence_prefixes;
+foreach my $sentence (keys %sentences) {
+  foreach my $len (1..length $sentence) {
+    $sentence_prefixes{substr $sentence, 0, $len} = 1;
+  }
+}
+
 # Read input plain text
+my $paragraph = "";
+while (defined ($_ = <>) || length $paragraph) {
+  chomp;
+  if (/^$/) {
+    # Locate all sentences present in the paragraph
+    for (my $i = 0; $i < length $paragraph; $i++) {
+      next if substr $paragraph, $i, 1 =~ /\s/;
+      my $sentence = "";
+      for (my $j = $i; $j < length $paragraph; $j++) {
+        next if substr $paragraph, $j, 1 =~ /\s/;
+        $sentence .= substr $paragraph, $j, 1;
+        last if not exists $sentence_prefixes{$sentence};
+        print "$sentence\n" if exists $sentences{$sentence};
+      }
+    }
+
+    $paragraph = "";
+  } else {
+    $paragraph .= "$_\n";
+  }
+}
 
 
 # Write output CoNLL-U
