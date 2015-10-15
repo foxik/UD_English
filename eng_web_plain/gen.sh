3446145783db6495621f9601c084e09de0c00056
Milan Straka <fox@ucw.cz>
2015-10-15 16:59:32 +0200

    Perform further normalization.

--- eng_web_plain/gen.sh
+++ eng_web_plain/gen.sh
@@ -4,4 +4,11 @@ sed 's#</*[^>]*>##g' ../eng_web_tbk/data/answers/source/source_original/*.xml >a
 sed '' ../eng_web_tbk/data/email/source/source_original/*.txt >email.txt
 sed 's#</*[^>]*>##g; s/&gt;/>/g; s/&lt;/</g; s/&amp;/\&/g; s/&quot;/"/g' ../eng_web_tbk/data/newsgroup/source/source_original/*.sgm >newsgroup.txt
 sed '' ../eng_web_tbk/data/reviews/source/source_original/*.txt >reviews.txt
-sed 's#</*[^>]*>##g; s/&gt;/>/g; s/&lt;/</g; s/&amp;/\&/g; s/&quot;/"/g' ../eng_web_tbk/data/weblog/source/source_original/*.sgm >weblog.txt
+sed 's#</*[^>]*>##g; s/&gt;/>/g; s/&lt;/</g; s/&amp;/\&/g; s/&quot;/"/g; s/&apos/'"'"'/g' ../eng_web_tbk/data/weblog/source/source_original/*.sgm >weblog.txt
+
+sed '
+  s/’/'"'"'/g; s/“/"/g; s/”/"/g;
+  s/Déjà/Deja/g
+' -i *.txt
+
+cat *.txt >../plain.txt
