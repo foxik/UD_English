c391c874655133aeae0d4134f116fcfd5178b26a
Milan Straka <fox@ucw.cz>
2015-10-16 15:46:50 +0200

    Add missing required formatting changes.

--- eng_web_plain/gen.sh
+++ eng_web_plain/gen.sh
@@ -7,8 +7,24 @@ sed '' ../eng_web_tbk/data/reviews/source/source_original/*.txt >reviews.txt
 sed 's#</*[^>]*>##g; s/&gt;/>/g; s/&lt;/</g; s/&amp;/\&/g; s/&quot;/"/g; s/&apos/'"'"'/g' ../eng_web_tbk/data/weblog/source/source_original/*.sgm >weblog.txt
 
 sed '
-  s/’/'"'"'/g; s/“/"/g; s/”/"/g;
-  s/Déjà/Deja/g
-' -i *.txt
+  s/[’´‘]/'"'"'/g; s/“/"/g; s/”/"/g
+  s/Ã³l/A3l/g
+  s/Υ/Y/g
+  s/[áà]/a/g
+  s/é/e/g
+  s/ç/c/g
+  s/£/L/g
+  s/♥/</g
+  s/·/*/g
+  s/[–—]/-/g
+  s/­/ /g
+  s/…/./g
 
-cat *.txt >../plain.txt
+  /e-mail address:  janette.elbertson@enron.com/ {N;N;N;N;s/Cafeteria is fine./Cafeteria is fin./; s/I better pass on the Comets game/I better pass on the Comets gam/}
+  s/^country taking toll of human lives, properties, etc.$/country taking toll of human lives, properties, etc. ./
+  s/Mailing them to the house iis fine./Mailing them to the house iis fin./
+  s/The best part is I got my whole bathroom remodeled for about the same price the other company'"'"'/The best part is I got my whole bathroom remodeled for about the same price the other compan'"'"'/
+
+  /^recette de cuisine | skype | tablature guitare | tarot | telecharger/,/^de la soupe recette de soupe/d
+
+' -i *.txt
