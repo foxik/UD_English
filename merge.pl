1a0735cac74bec39752f612a383dcec5f6c9c173
Milan Straka <fox@ucw.cz>
2015-10-16 15:47:12 +0200

    Find sentences on all input text simultaneously.

--- merge.pl
+++ merge.pl
@@ -47,62 +47,57 @@ my @conllu_space_yes = (0) x @conllu;
 my @conllu_space_no = (0) x @conllu;
 
 # Read input plain text
-my $paragraph = "";
-while (defined ($_ = <>) || length $paragraph) {
-  chomp;
-  if (/^$/) {
-    # Locate all sentences present in the paragraph
-    my @sentence_starts = (0) x length $paragraph;
-    my @sentence_ends = (0) x length $paragraph;
-    for (my $i = 0; $i < length $paragraph && substr($paragraph, $i, 1) =~ /\s/; $i++) { $sentence_ends[$i] = 1; }
-    for (my $i = length($paragraph) - 1; $i >= 0 && substr($paragraph, $i, 1) =~ /\s/; $i--) { $sentence_starts[$i] = 1; }
+my $input = "";
+while (<>) {
+  $input .= $_;
+}
 
-    # Mark all possible starts and ends
-    for (my $i = 0; $i < length $paragraph; $i++) {
-      next if substr($paragraph, $i, 1) =~ /\s/;
-      my $sentence = "";
-      for (my $j = $i; $j < length $paragraph; $j++) {
-        next if substr($paragraph, $j, 1) =~ /\s/;
-        $sentence .= substr $paragraph, $j, 1;
-        last if not exists $sentence_prefixes{$sentence};
-        if (exists $sentences{$sentence}) {
-          $sentence_starts[$i] = 1; for (my $k = $i - 1; $k >= 0 && substr($paragraph, $k, 1) =~ /\s/; $k--) { $sentence_starts[$k] = 1; }
-          $sentence_ends[$j] = 1; for (my $k = $j + 1; $k < length $paragraph && substr($paragraph, $k, 1) =~ /\s/; $k++) { $sentence_ends[$k] = 1; }
-        }
-      }
+# Locate all sentences present in the input
+my @sentence_starts = (0) x length $input;
+my @sentence_ends = (0) x length $input;
+for (my $i = 0; $i < length $input && substr($input, $i, 1) =~ /\s/; $i++) { $sentence_ends[$i] = 1; }
+for (my $i = length($input) - 1; $i >= 0 && substr($input, $i, 1) =~ /\s/; $i--) { $sentence_starts[$i] = 1; }
+
+# Mark all possible starts and ends
+for (my $i = 0; $i < length $input; $i++) {
+  next if substr($input, $i, 1) =~ /\s/;
+  my $sentence = "";
+  for (my $j = $i; $j < length $input; $j++) {
+    next if substr($input, $j, 1) =~ /\s/;
+    $sentence .= substr $input, $j, 1;
+    last if not exists $sentence_prefixes{$sentence};
+    if (exists $sentences{$sentence}) {
+      $sentence_starts[$i] = 1; for (my $k = $i - 1; $k >= 0 && substr($input, $k, 1) =~ /\s/; $k--) { $sentence_starts[$k] = 1; }
+      $sentence_ends[$j] = 1; for (my $k = $j + 1; $k < length $input && substr($input, $k, 1) =~ /\s/; $k++) { $sentence_ends[$k] = 1; }
     }
+  }
+}
 
-    # Fill spaces in all CoNLL-U occurrences of the found sentences
-    for (my $i = 0; $i < length $paragraph; $i++) {
-      next if substr($paragraph, $i, 1) =~ /\s/;
-      next if $i > 0 && !$sentence_ends[$i - 1];
-      my $sentence = "";
-      for (my $j = $i; $j < length $paragraph; $j++) {
-        next if substr($paragraph, $j, 1) =~ /\s/;
-        $sentence .= substr $paragraph, $j, 1;
-        last if not exists $sentence_prefixes{$sentence};
-        if (exists $sentences{$sentence} && ($j + 1 == length $paragraph || $sentence_starts[$j+1])) {
-          # Fill spaces in all CoNLL-U occurrences of the found sentence
-          foreach my $range (@{$sentences{$sentence}->{ranges}}) {
-            my ($start, $end) = ($range->{start}, $range->{end});
-            my $raw = substr($paragraph, $i, $j - $i + 1 + 1); # Add one additional character for SpaceAfter of last token
+# Fill spaces in all CoNLL-U occurrences of the found sentences
+for (my $i = 0; $i < length $input; $i++) {
+  next if substr($input, $i, 1) =~ /\s/;
+  next if $i > 0 && !$sentence_ends[$i - 1];
+  my $sentence = "";
+  for (my $j = $i; $j < length $input; $j++) {
+    next if substr($input, $j, 1) =~ /\s/;
+    $sentence .= substr $input, $j, 1;
+    last if not exists $sentence_prefixes{$sentence};
+    if (exists $sentences{$sentence} && ($j + 1 == length $input || $sentence_starts[$j+1])) {
+      # Fill spaces in all CoNLL-U occurrences of the found sentence
+      foreach my $range (@{$sentences{$sentence}->{ranges}}) {
+        my ($start, $end) = ($range->{start}, $range->{end});
+        my $raw = substr($input, $i, $j - $i + 1 + 1); # Add one additional character for SpaceAfter of last token
 
-            for (my $i = $start; $i <= $end; $i++) {
-              my $remove_token_re = "^\\s*" . join("\\s*", map { "\Q$_\E" } split //, $conllu[$i]->[1]);
-              if ($raw !~ s/$remove_token_re//) {
-                warn "Cannot match sentence $sentence at point $raw, range $start - $end";
-                last;
-              }
-              ($raw =~ /^\s/ ? \@conllu_space_yes : \@conllu_space_no)->[$i]++;
-            }
+        for (my $i = $start; $i <= $end; $i++) {
+          my $remove_token_re = "^\\s*" . join("\\s*", map { "\Q$_\E" } split //, $conllu[$i]->[1]);
+          if ($raw !~ s/$remove_token_re//) {
+            warn "Cannot match sentence $sentence at point $raw, range $start - $end";
+            last;
           }
+          ($raw =~ /^\s/ ? \@conllu_space_yes : \@conllu_space_no)->[$i]++;
         }
       }
     }
-
-    $paragraph = "";
-  } else {
-    $paragraph .= "$_\n";
   }
 }
 
